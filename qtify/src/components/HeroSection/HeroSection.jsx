6e041c5b55cd0a13eebf6e52dc8dd8a33f876cdb
subhash rao <subba714159@gmail.com>
2024-05-31 14:23:29 +0530

    Test commit

--- qtify/src/components/HeroSection/HeroSection.jsx
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -2,6 +2,7 @@ import './HeroSection.css';
 const HeroSection = () => {
   return (
     <div className="hero-section">
+      <div>100 Thousand Songs, ad-free</div>
       <img
         src="HeroImage.png"
         alt="100 Thousand Songs, ad-free"
